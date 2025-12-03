5ec90d4411e674af32fab52f1283762db990969c
eclipse021 <yahang2008@naver.com>
2025-12-03 23:26:17 +0900

    init: 프로젝트 생성

---
+++ src/main/java/org/project/ssogssog/SsogssogApplication.java
@@ -0,0 +1,13 @@
+package org.project.ssogssog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsogssogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsogssogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/ssogssog/SsogssogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.ssogssog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsogssogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
