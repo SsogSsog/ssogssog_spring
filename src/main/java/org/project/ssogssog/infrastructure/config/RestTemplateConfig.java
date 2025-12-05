3810dc46edf131bd1ffa5709234ff20d6cffd126
eclipse021 <yahang2008@naver.com>
2025-12-05 19:12:36 +0900

    feat: RestTemplateConfig 생성

---
+++ src/main/java/org/project/ssogssog/infrastructure/config/RestTemplateConfig.java
@@ -0,0 +1,21 @@
+package org.project.ssogssog.infrastructure.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .connectTimeout(Duration.ofSeconds(10))  // ✅ connectTimeout
+                .readTimeout(Duration.ofSeconds(10))     // ✅ readTimeout
+                .build();
+    }
+
+}
