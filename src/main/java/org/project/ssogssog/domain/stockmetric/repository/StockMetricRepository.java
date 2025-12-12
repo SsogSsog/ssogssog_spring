8b3d924bf7b2df53d9f823e44277aac7ff86fdbb
eclipse021 <yahang2008@naver.com>
2025-12-12 22:43:35 +0900

    feat: StockMetricRepository 생성

---
+++ src/main/java/org/project/ssogssog/domain/stockmetric/repository/StockMetricRepository.java
@@ -0,0 +1,9 @@
+
+package org.project.ssogssog.domain.stockmetric.repository;
+
+import org.project.ssogssog.domain.stockmetric.entity.StockMetric;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockMetricRepository extends JpaRepository<StockMetric, Long> {
+}
+
