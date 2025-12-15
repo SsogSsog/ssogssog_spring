37d89b4a087a47598c1fdf4f42bae132844a90d6
eclipse021 <yahang2008@naver.com>
2025-12-15 15:07:07 +0900

    fix: 사용하지 않은 import 제거

--- src/main/java/org/project/ssogssog/presentation/controller/stockmetric/StockMetricController.java
+++ src/main/java/org/project/ssogssog/presentation/controller/stockmetric/StockMetricController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.project.ssogssog.application.stockmetric.api.StockMetricService;
-import org.project.ssogssog.application.stockmetric.collect.StockMetricRefreshService;
 import org.project.ssogssog.presentation.common.ApiResponse;
 import org.project.ssogssog.presentation.controller.stockmetric.dto.StockMetricRequest;
 import org.project.ssogssog.presentation.controller.stockmetric.dto.StockMetricResponse;
