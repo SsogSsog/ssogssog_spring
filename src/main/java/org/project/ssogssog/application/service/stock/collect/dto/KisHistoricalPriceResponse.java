8a2217e453cc37b7d00d1bc158c1feab445e5541
eclipse021 <yahang2008@naver.com>
2025-12-21 06:31:38 +0900

    refactor: 과거 KIS 정보를 가져오는 dto 이름 변경

--- src/main/java/org/project/ssogssog/application/service/stock/collect/dto/KisHistoricalPriceResponse.java
+++ src/main/java/org/project/ssogssog/application/service/stock/collect/dto/KisHistoricalPriceResponse.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-public class KisDailyPriceResponse {
+public class KisHistoricalPriceResponse {
     @JsonProperty("output1")
     private Output1 output1; // 대표 정보
 
