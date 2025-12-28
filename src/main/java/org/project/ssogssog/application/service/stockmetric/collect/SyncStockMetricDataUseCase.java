d68eea26d719fe1d58f8df0b6577b4842a5cca1d
eclipse021 <yahang2008@naver.com>
2025-12-29 01:35:22 +0900

    refactor: StockMetricWriter 생성 및 stockMetric 저장 메소드 분리

--- src/main/java/org/project/ssogssog/application/service/stockmetric/collect/SyncStockMetricDataUseCase.java
+++ src/main/java/org/project/ssogssog/application/service/stockmetric/collect/SyncStockMetricDataUseCase.java
@@ -2,6 +2,7 @@ package org.project.ssogssog.application.service.stockmetric.collect;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.project.ssogssog.application.service.stockmetric.writer.StockMetricWriter;
 import org.project.ssogssog.domain.stock.entity.DailyPrice;
 import org.project.ssogssog.domain.stock.entity.Stock;
 import org.project.ssogssog.domain.stock.entity.StockFinancial;
@@ -25,10 +26,8 @@ import java.util.List;
 public class SyncStockMetricDataUseCase {
 
     private final StockRepository stockRepository;
-    private final StockMetricRepository stockMetricRepository;
 
-    private final StockFinancialRepository stockFinancialRepository;
-    private final DailyPriceRepository dailyPriceRepository;
+    private final StockMetricWriter stockMetricWriter;
 
 
     /**
@@ -38,77 +37,10 @@ public class SyncStockMetricDataUseCase {
 
         List<Stock> stocks = stockRepository.findAll();
         for (Stock stock : stocks) {
-            refreshMetricForStock(stock); // 주의) 현재 같은 클래스이므로 @Transactional이 무시되는 상황이므로 추후 원자적인 처리로 단위 고려하기
+            stockMetricWriter.refreshMetricForStock(stock); // 주의) 현재 같은 클래스이므로 @Transactional이 무시되는 상황이므로 추후 원자적인 처리로 단위 고려하기
         }
 
     }
 
-
-    /**
-     * 특정 종목 하나의 지표를 계산해서 저장
-     */
-    @Transactional
-    public void refreshMetricForStock(Stock stock) {
-
-
-        DailyPrice latestDailyPrice = dailyPriceRepository
-                .findTopByStockOrderByDateDesc(stock)
-                .orElse(null);
-
-        StockFinancial latestStockFinancial = stockFinancialRepository
-                .findTopByStockOrderByYearDescQuarterDesc(stock)
-                .orElse(null);
-
-        if (latestDailyPrice == null || latestStockFinancial == null) {
-            log.warn("❌ StockMetric 계산 불가 - 기초 데이터 부족. stockId={}, code={}",
-                    stock.getId(), stock.getStockCode());
-            return;
-        }
-
-        final int latestYear = latestStockFinancial.getYear();          // 최근 년도
-        final String latestQuarter = latestStockFinancial.getQuarter(); // 최근 분기
-        final YearQuarter prevQuarter = YearQuarter.prevQuarter(latestYear, latestQuarter);   // 직전 분기 YearQuarter
-        final YearQuarter prevYear = YearQuarter.prevYear(latestYear, latestQuarter);         // 작년 YearQuarter
-
-        StockFinancial prevQuarterStockFinancial = stockFinancialRepository
-                .findByStockIdAndYearAndQuarter(stock.getId(), prevQuarter.year(), prevQuarter.quarter())
-                .orElse(null);
-
-        StockFinancial prevYearStockFinancial = stockFinancialRepository
-                .findByStockIdAndYearAndQuarter(stock.getId(), prevYear.year(), prevYear.quarter())
-                .orElse(null);
-
-        // currentPrice, marketCap
-        // per, roe, netProfitMargin, debtRatio,
-        // salesGrowthQoQ, salesGrowthYoY, netProfitGrowthQoQ, netProfitGrowthYoY,
-        // dividendYield, foreignOwnershipRate, return3M/6M/12M 계산
-        MetricValues metricValues = StockMetricCalculator.calculate(
-                stock, latestDailyPrice, latestStockFinancial,prevQuarterStockFinancial, prevYearStockFinancial);
-
-
-        StockMetric metric = stockMetricRepository
-                .findByStock(stock)
-                .orElseGet(() -> StockMetric.builder().stock(stock).build());
-
-        metric.updateAll(
-                metricValues.currentPrice(),
-                metricValues.marketCap(),
-                metricValues.per(),
-                metricValues.roe(),
-                metricValues.netProfitMargin(),
-                metricValues.debtRatio(),
-                metricValues.salesGrowthQoQ(),
-                metricValues.salesGrowthYoY(),
-                metricValues.netProfitGrowthQoQ(),
-                metricValues.netProfitGrowthYoY(),
-                metricValues.dividendYield(),
-                metricValues.foreignOwnershipRate(),
-                metricValues.return3M(),
-                metricValues.return6M(),
-                metricValues.return12M()
-        );
-
-        stockMetricRepository.save(metric);
-    }
 }
 
