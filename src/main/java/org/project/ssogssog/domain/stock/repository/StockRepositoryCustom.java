308a16b8dbc79366af4e3f9a9c3672d0a9a85644
eclipse021 <yahang2008@naver.com>
2026-01-02 04:47:08 +0900

    feat: 종목별 주식 목록 조회하는 쿼리 구현 - 종가 내림차순 정렬

--- src/main/java/org/project/ssogssog/domain/stock/repository/StockRepositoryCustom.java
+++ src/main/java/org/project/ssogssog/domain/stock/repository/StockRepositoryCustom.java
@@ -1,9 +1,15 @@
 package org.project.ssogssog.domain.stock.repository;
 
+import org.project.ssogssog.domain.stock.vo.StockItemDTO;
 import org.project.ssogssog.domain.stock.vo.ThemeItemDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface StockRepositoryCustom {
     List<ThemeItemDTO> getThemeStockStats();
+
+    Page<StockItemDTO> getStocksForThemeOrderByClosePrice(String theme, Pageable pageable);
+
 }
