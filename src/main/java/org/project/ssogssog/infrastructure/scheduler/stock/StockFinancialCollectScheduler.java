a491d97c4322f6c274386bab4fc5abf2790c2ca4
eclipse021 <yahang2008@naver.com>
2026-01-04 04:33:25 +0900

    feat: 개별적으로 스케쥴러 제어하도록 속성 변경 및 스케쥴러 등록 여부 검사 로그 추가

--- src/main/java/org/project/ssogssog/infrastructure/scheduler/stock/StockFinancialCollectScheduler.java
+++ src/main/java/org/project/ssogssog/infrastructure/scheduler/stock/StockFinancialCollectScheduler.java
@@ -1,5 +1,6 @@
 package org.project.ssogssog.infrastructure.scheduler.stock;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.project.ssogssog.application.service.stock.usecase.CollectFinancialsUseCase;
@@ -10,13 +11,19 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 @ConditionalOnProperty(
-        name = "scheduler.today-price.enabled", // 수집 인스턴스에만 해당 값 켜놓기
+        name = "scheduler.financial.enabled", // 수집 인스턴스에만 해당 값 켜놓기
         havingValue = "true",
         matchIfMissing = false
 )
 public class StockFinancialCollectScheduler {
 
     private final CollectFinancialsUseCase collectFinancialsUseCase;
+
+    // 스케쥴러가 빈에 등록됐는지 여부 검사
+    @PostConstruct
+    public void init() {
+        log.info("[StockFinancialCollectScheduler] 로드 완료");
+    }
 
     // TODO 하루에 한 번 새로운 분기의 재무제표 확인 및 업데이트 하기
 }
