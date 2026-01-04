852929ed29242813bc190ad3eb0d4d3893a467e1
eclipse021 <yahang2008@naver.com>
2026-01-05 06:26:00 +0900

    feat: StockAdapter 구현

--- src/main/java/org/project/ssogssog/application/service/stock/port/StockPort.java
+++ src/main/java/org/project/ssogssog/application/service/stock/port/StockPort.java
@@ -1,7 +1,8 @@
 package org.project.ssogssog.application.service.stock.port;
 
 public interface StockPort {
-
-
     String fetchSector(String stockCode);
+
+    // OpenDart 에서 가져온 전체 XML 데이터 전달
+    byte[] getCorpCodeZip();
 }
