d724ccc1d900907d688172acabe821a445ffb617
eclipse021 <yahang2008@naver.com>
2026-01-05 06:06:53 +0900

    chore: 인메모리 캐시 의존성 추가

---
+++ src/main/java/org/project/ssogssog/infrastructure/config/CacheConfig.java
@@ -0,0 +1,20 @@
+package org.project.ssogssog.infrastructure.config;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public Cache<String, String> kisTokenCache() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(Duration.ofHours(12))
+                .maximumSize(10)
+                .build();
+    }
+}
