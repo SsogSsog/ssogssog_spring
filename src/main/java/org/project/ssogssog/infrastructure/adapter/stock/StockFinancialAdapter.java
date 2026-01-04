c4d1914fe5835d49d981144af831990c57c72f2e
eclipse021 <yahang2008@naver.com>
2026-01-05 06:06:31 +0900

    feat: 재무제표 adapter에 openDartRateLimiter 적용

--- src/main/java/org/project/ssogssog/infrastructure/adapter/stock/StockFinancialAdapter.java
+++ src/main/java/org/project/ssogssog/infrastructure/adapter/stock/StockFinancialAdapter.java
@@ -3,14 +3,17 @@ package org.project.ssogssog.infrastructure.adapter.stock;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.google.common.util.concurrent.RateLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.project.ssogssog.application.service.stock.port.StockFinancialPort;
+import org.project.ssogssog.infrastructure.client.ksi.KISClient;
 import org.project.ssogssog.infrastructure.client.opendart.OpenDartClient;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Slf4j
-@RequiredArgsConstructor
 @Component
 public class StockFinancialAdapter implements StockFinancialPort {
 
@@ -18,12 +21,25 @@ public class StockFinancialAdapter implements StockFinancialPort {
     private final ObjectMapper objectMapper;
 
     private final OpenDartClient openDartClient;
+    private final RateLimiter rateLimiter;
+
+    // 주의!
+    // @Qualifier 어노테이션 사용 시 생성자를 직접 만들어야 에러가 안 생긴다..
+    public StockFinancialAdapter(ObjectMapper objectMapper,
+                             OpenDartClient openDartClient,
+                             @Qualifier("openDartRateLimiter") RateLimiter rateLimiter) {
+        this.objectMapper = objectMapper;
+        this.openDartClient = openDartClient;
+        this.rateLimiter = rateLimiter;
+    }
 
 
     @Override
     public JsonNode getFinancialInfo(String corpCode, Integer year, String reportCode) {
 
         try {
+            rateLimiter.acquire();
+
             String response = openDartClient.getFinancialInfo(corpCode, year, reportCode);
             JsonNode root = objectMapper.readTree(response);
             return root;
