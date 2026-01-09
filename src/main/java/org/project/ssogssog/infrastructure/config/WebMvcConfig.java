f7e5f4294e2b653ced72a63c1fd8d8e455bb6bca
eclipse021 <yahang2008@naver.com>
2026-01-10 05:05:31 +0900

    feat: WebMvcConfig에 UuidInterceptor 등록

---
+++ src/main/java/org/project/ssogssog/infrastructure/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package org.project.ssogssog.infrastructure.config;
+
+import lombok.RequiredArgsConstructor;
+import org.project.ssogssog.infrastructure.interceptor.UuidInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final UuidInterceptor uuidInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(uuidInterceptor)
+                //.addPathPatterns("/**") // 모든 API 검사
+                //.excludePathPatterns("/members/register"); // 등록 API는 검사 제외
+                .addPathPatterns("/members/**") // 개발, 테스트 편의성을 member 관련만 인증
+                .excludePathPatterns("/members/register");
+    }
+}
