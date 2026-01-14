4255de9e04a1080754d8bba08467a926e5c26368
eclipse021 <yahang2008@naver.com>
2026-01-15 01:11:28 +0900

    fix: 로그인 후 attribute 값 변경

--- src/main/java/org/project/ssogssog/infrastructure/interceptor/UuidInterceptor.java
+++ src/main/java/org/project/ssogssog/infrastructure/interceptor/UuidInterceptor.java
@@ -29,7 +29,7 @@ public class UuidInterceptor implements HandlerInterceptor {
         }
 
         // 검증된 UUID를 request attribute에 저장하여 다운스트림에서 사용 가능하도록 함
-        request.setAttribute("memberId", uuid);
+        request.setAttribute("memberUuId", uuid);
 
         return true;
     }
